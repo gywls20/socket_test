58761c6e6537a0606ff1bcd87cc7e42b5330306b
gywls20 <koj858@naver.com>
2023-04-06 00:08:16 +0900

    test

---
+++ app.js
@@ -0,0 +1,59 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+app.io = require('socket.io')();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+// socekt.io 로직은 module.exports = app; 바로 직전에 입력
+app.io.on('connection', function(socket){
+  console.log('socket is communicating');
+  socket.emit('usercount', app.io.engine.clientsCount);
+  socket.on('message', (msg) => {
+    //msg에는 클라이언트에서 전송한 매개변수가 들어온다. 이러한 매개변수의 수에는 제한이 없다.
+    console.log('Message received: ' + msg);
+
+    // io.emit으로 연결된 모든 소켓들에 신호를 보낼 수 있다.
+    app.io.emit('message', msg);
+    
+  });
+});
+
+module.exports = app;
